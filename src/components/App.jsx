c6619bde191eaee94d0359c3d5a87eb492ca13d6
lahmzy <103523527+lahmzy@users.noreply.github.com>
2022-08-14 23:23:46 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
+  function handleDelete(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((items, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item, index) => {
+            return (
+              <ToDoItem
+                key={index}
+                id={index}
+                text={item}
+                delete={handleDelete}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
